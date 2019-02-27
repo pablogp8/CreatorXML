26823252620f6304cecde6e6c6885e7024fcd887
Pablo <pablogp8@gmail.com>
2019-02-27 04:43:08 -0600

    segundo commit

--- src/creatorxml/CreatorXML.java
+++ src/creatorxml/CreatorXML.java
@@ -16,6 +16,7 @@ public class CreatorXML {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
